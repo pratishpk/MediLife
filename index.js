78089532e17101a4b5b196849c459cdc7ddc9c8d
pratishpk <72383158+pratishpk@users.noreply.github.com>
2023-12-22 18:49:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+// JavaScript functions to open and close the form
+function openForm() {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myModal").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+
+
+// JavaScript functions for slider
+
+
+let items = document.querySelectorAll('.slider .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+let active = 3;
+
+function loadShow() {
+    let stt = 0;
+    items[active].style.transform = `none`;
+    items[active].style.zIndex = 1;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
+    for (var i = active + 1; i < items.length; i++) {
+        stt++;
+        items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+   
+    stt = 0;
+    for (var i = active - 1; i >= 0; i--) {
+        stt++;
+        items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+}
+
+loadShow();
+
+next.onclick = function(){
+    active = active + 1< items.length ? active + 1 : active;
+    loadShow();
+
+}
+
+prev.onclick = function(){
+    active = active - 1< items.length ? active - 1 : active;
+    loadShow();
+
+}
+
